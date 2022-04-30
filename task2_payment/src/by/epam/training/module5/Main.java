0ad1e85586dfa8601f8c2ece47a8fea295e94a7e
Daria <dashatur100500@gmail.com>
2022-04-30 15:30:12 +0300

    edit

---
+++ task2_payment/src/by/epam/training/module5/Main.java
@@ -0,0 +1,26 @@
+package by.epam.training.module5;
+/*
+ * Задача 2.  
+ * Создать класс Payment с внутренним классом, с помощью объектов которого можно сформировать покупку из 
+ * нескольких товаров.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Payment payment = new Payment();
+		Payment.Product product1 = payment.new Product("Boots", 53.4);
+		Payment.Product product2 = payment.new Product("T-shirt", 13.73);
+		Payment.Product product3 = payment.new Product("Trousers", 25.89);
+		Payment.Product product4 = payment.new Product("Heels", 30.95);
+		Payment.Product product5 = payment.new Product("Coat", 78.45);
+		payment.addProduct(product1);
+		payment.addProduct(product2);
+		payment.addProduct(product3);
+		payment.addProduct(product4);
+		payment.addProduct(product5);
+		
+		payment.showCart();
+		
+	}
+
+}
---
+++ task2_payment/src/by/epam/training/module5/Payment.java
@@ -0,0 +1,165 @@
+package by.epam.training.module5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Payment {
+	private List<Product> cart;
+	
+	public Payment() {
+		cart = new ArrayList<Product>();
+	};
+	
+	public Payment(List<Product> cart) {
+		this.cart = cart;
+	}
+	
+	public void addProduct(Product product) {
+		cart.add(product);
+	}
+	
+	public void deleteProduct(Product product) {
+		cart.remove(product);
+	}
+	
+	public double sum() {
+		double sum = 0.0;
+		
+		for(int i = 0; i<cart.size(); i++) {
+			sum+=cart.get(i).getCost();
+		}
+		
+		return sum;
+	}
+	
+	public List<Product> getCart() {
+		return cart;
+	}
+
+	public void setCart(List<Product> cart) {
+		this.cart = cart;
+	}
+
+	@Override
+	public String toString() {
+		return "Payment [cart=" + cart + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cart == null) ? 0 : cart.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Payment other = (Payment) obj;
+		if (cart == null) {
+			if (other.cart != null) {
+				return false;
+			}
+		} else if (!cart.equals(other.cart)) {
+			return false;
+		}
+		return true;
+	}
+
+	public void showCart() {
+		for(int i = 0; i<cart.size(); i++) {
+			System.out.println("Название товара: "+cart.get(i).getName()+", цена: "+cart.get(i).getCost());
+			System.out.println("------------------------------------------------------");
+		}
+	}
+	
+	public class Product{
+		private String name;
+		private double cost;
+		
+		public Product() {};
+		
+		public Product(String name, double cost) {
+			if(cost>0) {
+				this.name = name;
+				this.cost = cost;
+			} else {
+				throw new IllegalArgumentException();
+			}
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public double getCost() {
+			return cost;
+		}
+
+		public void setCost(double cost) {
+			if(cost>0) {
+				this.cost = cost;
+			} else {
+				throw new IllegalArgumentException();
+			}
+			
+		}
+
+		@Override
+		public String toString() {
+			return "Product [name=" + name + ", cost=" + cost + "]";
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			long temp;
+			temp = Double.doubleToLongBits(cost);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			Product other = (Product) obj;
+			if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost)) {
+				return false;
+			}
+			if (name == null) {
+				if (other.name != null) {
+					return false;
+				}
+			} else if (!name.equals(other.name)) {
+				return false;
+			}
+			return true;
+		}
+
+		
+		
+	}
+}
